8bf492d8bb303575688635a00b9c2e14669608fd
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-04 18:23:34 +0530

    added the new content in the file

--- StudentInformation/StudentInformation/ContentView.swift
+++ StudentInformation/StudentInformation/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Text("Name:- Ritesh Dhamale")
+            Text("Age: 24")
+            Text("Learning swift UI")
+            Image(systemName: "heart")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .foregroundColor(Color.red)
+            Text("Welcome to the iOS Development")
         }
         .padding()
     }
