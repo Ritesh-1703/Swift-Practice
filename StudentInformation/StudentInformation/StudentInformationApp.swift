92a2cce5bfc8a26f81d504ddfa8630ca02749344
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-04 17:23:53 +0530

    Application created

---
+++ StudentInformation/StudentInformation/StudentInformationApp.swift
@@ -0,0 +1,18 @@
+//
+//  StudentInformationApp.swift
+//  StudentInformation
+//
+//  Created by Admin on 04/08/26.
+//
+
+import SwiftUI
+
+@main
+struct StudentInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
