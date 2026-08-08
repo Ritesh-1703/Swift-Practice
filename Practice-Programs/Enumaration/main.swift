c521e6db7b0f55e5d826d3c4923c98ac99d8da25
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-08 15:26:35 +0530

    Added the enumaration

---
+++ Practice-Programs/Enumaration/main.swift
@@ -0,0 +1,22 @@
+enum Direction{
+    case north
+    case south
+    case east
+    case west
+}
+
+
+let move = Direction.south
+
+
+switch move {
+    case .north:
+        print("Moving north")
+    case .south:
+        print("Moving south")
+    case .east:
+        print("Moving east")
+    case .west:
+        print("Moving west")    
+}
+
