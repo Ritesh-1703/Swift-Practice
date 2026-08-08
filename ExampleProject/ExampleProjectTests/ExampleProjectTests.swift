68107ca1d2cb5534c2bb97b9cb4237088a093a3f
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-08 14:38:36 +0530

    Updated the student information project

---
+++ ExampleProject/ExampleProjectTests/ExampleProjectTests.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleProjectTests.swift
+//  ExampleProjectTests
+//
+//  Created by Admin on 07/08/26.
+//
+
+import Testing
+
+struct ExampleProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
