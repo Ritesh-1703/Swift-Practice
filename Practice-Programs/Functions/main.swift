cf43ea09c793371e2f32c97de3e8b29db2c72c60
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-08 15:15:04 +0530

    Added parmeted funtion

--- Practice-Programs/Functions/main.swift
+++ Practice-Programs/Functions/main.swift
@@ -3,3 +3,16 @@ func greet(){
 }
 
 greet()
+
+func greetWithName(name: String){
+    print("Hello \(name) from swift functions")
+}
+
+greetWithName(name: "John")
+
+func addNumbers(num1: Int, num2: Int) -> Int{
+    return num1 + num2
+}
+
+let sum = addNumbers(num1: 5, num2: 10)
+print("Sum is", sum)
