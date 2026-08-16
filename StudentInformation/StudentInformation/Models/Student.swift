094a89b26210907250f901b65d1b5ce19e0f44a2
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-16 13:16:48 +0530

    Added the array in to the student list file

--- StudentInformation/StudentInformation/Models/Student.swift
+++ StudentInformation/StudentInformation/Models/Student.swift
@@ -7,17 +7,9 @@
 
 import Foundation
 
-struct Student{
+struct Student: Identifiable{
     let id : Int
     let name : String
     let course : String
     let semester : String
 }
-
-let students = [
-    Student(id: 1 , name: "Rohit Sharma", course: "BSC-CS", semester: "Semester 2"),
-    Student(id: 2 , name: "Virat Kholi", course: "BCA", semester: "Semester 6"),
-    Student(id: 3, name: "Ajinkya Rahane", course: "MCA", semester: "Semester 3"),
-    Student(id: 4 , name: "MS Dhoni", course: "MCA", semester: "Semester 4"),
-    Student(id: 5 , name: "KL Rahul", course: "MBA", semester: "Semester 1")
-]
--- StudentInformation/StudentInformation/Views/Student/StudentListView.swift
+++ StudentInformation/StudentInformation/Views/Student/StudentListView.swift
@@ -1,13 +1,22 @@
-//
-//  StudentListView.swift
-//  StudentInformation
-//
-//  Created by Admin on 16/08/26.
-//
+    //
+    //  StudentListView.swift
+    //  StudentInformation
+    //
+    //  Created by Admin on 16/08/26.
+    //
 
 import SwiftUI
 
 struct StudentListView: View {
+    
+    let students = [
+        Student(id: 1 , name: "Rohit Sharma", course: "BSC-CS", semester: "Semester 2"),
+        Student(id: 2 , name: "Virat Kholi", course: "BCA", semester: "Semester 6"),
+        Student(id: 3, name: "Ajinkya Rahane", course: "MCA", semester: "Semester 3"),
+        Student(id: 4 , name: "MS Dhoni", course: "MCA", semester: "Semester 4"),
+        Student(id: 5 , name: "KL Rahul", course: "MBA", semester: "Semester 1")
+    ]
+    
     var body: some View {
         
         List{
