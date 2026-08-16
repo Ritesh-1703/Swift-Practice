9ca9db2459bce6fd3960dea2a3afdd86ea744157
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-16 11:13:54 +0530

    modify the profile view

--- StudentInformation/StudentInformation/Views/Profile/StudentProfileView.swift
+++ StudentInformation/StudentInformation/Views/Profile/StudentProfileView.swift
@@ -9,12 +9,33 @@ import SwiftUI
 
 struct StudentProfileView: View {
     
-    @State private var studentName: = "Ritesh Dhamale"
+    @State private var studentName = "Ritesh Dhamale"
     @State private var email = "ritesh@gmail.com"
-    
-    
+    @State private var password = ""
+    @State private var cource = "MCA"
+    @State private var semester = "Semester 3"
+    @State private var birthDate =  Date()
+    @State private var notificationsEnabled = true
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            
+            VStack(alignment: .leading, spacing: 20){
+                
+                Text("Student Profile")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("Update you personal information")
+                    .foregroundStyle(.secondary)
+                
+                Group{
+                    
+                    Text("Student Name")
+            
+                }
+            }
+        }
     }
 }
 
