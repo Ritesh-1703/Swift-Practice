a07e6bfd2d09cf6129d48a58ea43dea6e1f73fba
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-16 11:08:40 +0530

    added the student profile view

---
+++ StudentInformation/StudentInformation/Views/Profile/StudentProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  StudentProfileView.swift
+//  StudentInformation
+//
+//  Created by Admin on 16/08/26.
+//
+
+import SwiftUI
+
+struct StudentProfileView: View {
+    
+    @State private var studentName: = "Ritesh Dhamale"
+    @State private var email = "ritesh@gmail.com"
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentProfileView()
+}
