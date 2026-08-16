fe9a2b3cdc61975b36cd34ae52bd90577097abeb
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-16 12:38:03 +0530

    Created the student list file

---
+++ StudentInformation/StudentInformation/Views/Student/StudentListView.swift
@@ -0,0 +1,18 @@
+//
+//  StudentListView.swift
+//  StudentInformation
+//
+//  Created by Admin on 16/08/26.
+//
+
+import SwiftUI
+
+struct StudentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StudentListView()
+}
