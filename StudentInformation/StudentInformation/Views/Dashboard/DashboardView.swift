e4ee0e792b5ae29e93eae543947a0b013748f75d
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-16 09:11:47 +0530

    added the dashboard View file in the main file

---
+++ StudentInformation/StudentInformation/Views/Dashboard/DashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift
+//  StudentInformation
+//
+//  Created by Admin on 16/08/26.
+//
+
+import SwiftUI
+
+struct DashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView()
+}
