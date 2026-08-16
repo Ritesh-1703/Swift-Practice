ed006fbe7bcb36cf0adb413f60a2d1ca79bf7dcc
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-08-16 12:59:58 +0530

    added the student model

---
+++ StudentInformation/StudentInformation/Models/Student.swift
@@ -0,0 +1,15 @@
+//
+//  Student.swift
+//  StudentInformation
+//
+//  Created by Admin on 16/08/26.
+//
+
+import Foundation
+
+struct Student{
+    let id : Int
+    let name : String
+    let course : String
+    let semester : String
+}
